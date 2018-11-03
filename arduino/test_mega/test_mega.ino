1c7f5d9808c122d6f7935c3cc7f45bf72da2de1c
Miles <milesave@buffalo.edu>
2018-11-03 18:18:36 -0400

    test files serial to wifi
    test files added for sending packets from arduino to itself to check if the packet loss prevention is working correctly

--- arduino/test_mega/test_mega.ino
+++ arduino/test_mega/test_mega.ino
@@ -1,22 +1,59 @@
+#include <DS3231.h>
 
+DS3231 rtc(SDA,SCL);
+
+Time t;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial1.begin(9600);
 
-  Serial.print("mega active");
-  Serial.print("waiting for wifi");
+  //start real time clock
+  rtc.begin();
   
-  while(Serial1.available() == 0){
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.print(Serial1.readString());
+  //rtc.setTime(5,30,0);
+  //rtc.setDate(3,11,2018);
 }
+
+float packet[12];
+
+// set data integrity flags
+packet[0] = 13.0;
+packet[11] = 13.0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial1.available())
-    Serial.print(Serial1.readString());
+  
+  // read/save data from pH sensor
+  packet[1] = 1.0;
+  
+  // read/save data from tds sensor
+  packet[2] = 1.0;
+  
+  // read/save data from temp sensor
+  packet[3] = 1.0;
+  
+  // read/save data from flow meter
+  packet[4] = 1.0;
+  
+  // read time from rtc and insert into packet
+  t = rtc.getTime();
+  
+  packet[5] = t.hour;
+  packet[6] = t.min;
+  packet[7] = t.sec;
+  packet[8] = t.year;
+  packet[9] = t.mon;
+  packet[10] = t.date;
+  
+  Serial.print("sending packet..");
+  Serial1.write(
+  
+  // Wait one second before repeating :)
+  delay (3000);
+
+
+
+  
 }
