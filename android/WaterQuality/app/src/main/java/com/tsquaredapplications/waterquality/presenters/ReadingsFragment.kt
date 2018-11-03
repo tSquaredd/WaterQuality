4d957702943f15dc4536a483cac6656862114ea2
tylerturnbull <tsquarredapplications@gmail.com>
2018-11-03 18:22:04 -0400

    Two of the values displayed on ReadingsFragment

--- android/WaterQuality/app/src/main/java/com/tsquaredapplications/waterquality/presenters/ReadingsFragment.kt
+++ android/WaterQuality/app/src/main/java/com/tsquaredapplications/waterquality/presenters/ReadingsFragment.kt
@@ -10,9 +10,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.robinhood.spark.animation.MorphSparkAnimator
+import com.robinhood.ticker.TickerUtils
 
 import com.tsquaredapplications.waterquality.R
+import com.tsquaredapplications.waterquality.adapters.SparkViewFloatAdapter
+import com.tsquaredapplications.waterquality.data.WaterData
 import com.tsquaredapplications.waterquality.viewmodel.MainActivityViewModel
+import kotlinx.android.synthetic.main.fragment_readings.*
 
 class ReadingsFragment : Fragment() {
 
@@ -26,13 +31,56 @@ class ReadingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_readings, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        flow_ticker.setCharacterLists(TickerUtils.provideNumberList())
+        temp_ticker.setCharacterLists(TickerUtils.provideNumberList())
+
+        flow_spark_view.sparkAnimator = MorphSparkAnimator()
+        temp_spark_view.sparkAnimator = MorphSparkAnimator()
+    }
+
     override fun onResume() {
         super.onResume()
         // observe data flow
-        viewModel.getDataStream().observe(this, Observer {
-            for(reading in it)
-                Log.i("ReadingsFragment", "onResume: ${reading.toString()}")
-        })
+        viewModel.getDataStream().observe(this, WaterDataObserver())
     }
 
+    inner class WaterDataObserver : Observer<List<WaterData>> {
+        private val flowList = arrayListOf<Float>()
+        private val tempList = arrayListOf<Float>()
+
+        var flowAdapter: SparkViewFloatAdapter? = null
+
+        override fun onChanged(it: List<WaterData>?) {
+            it?.let {
+                for (reading in it) {
+                   if (flowAdapter == null)
+                        flowList.add(reading.flow)
+
+                    if (temp_spark_view.adapter == null)
+                        tempList.add(reading.temp)
+                }
+
+                // FLOW
+                flow_ticker.text = (it[it.size - 1].flow.toString())
+
+                if (flowAdapter == null){
+                    flowAdapter = SparkViewFloatAdapter(flowList)
+                    flow_spark_view.adapter = flowAdapter
+                }
+                else
+                    flowAdapter!!.addReading(it[it.size - 1].flow)
+
+                // TEMP
+                temp_ticker.text = (it[it.size - 1].temp.toString())
+                if (temp_spark_view.adapter == null)
+                    temp_spark_view.adapter = SparkViewFloatAdapter(tempList)
+                else
+                    (temp_spark_view.adapter as SparkViewFloatAdapter).addReading(it[it.size - 1].temp)
+
+            }
+        }
+
+    }
 }
