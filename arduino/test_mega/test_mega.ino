d67b97a9fb16cfc3f16043dac973c618dfc7397e
Miles <milesave@buffalo.edu>
2018-11-04 07:05:07 -0500

    pH sensor done
    pH sensor implemented and tested

--- arduino/test_mega/test_mega.ino
+++ arduino/test_mega/test_mega.ino
@@ -2,6 +2,11 @@
 #include <OneWire.h>
 #include <DS3231.h>
 
+//ph sensor
+int ph_pin = A2;
+int measure;
+double voltage;
+float po;
 //tds sensor
 #define TDS_SENSOR_PIN A1
 #define VREF 5.0
@@ -99,7 +104,7 @@ void setup() {
   sensors.begin();
   
   //delay
-  delay(5000);
+  delay(60000);
   Serial.println("startup complete");
   
   //rtc.setTime(0,11,0);
@@ -107,35 +112,38 @@ void setup() {
 }
 
 DeviceAddress t_address;
+
 int getMedianNum(int bArray[], int iFilterLen){
- int bTab[iFilterLen];
- for (byte i = 0; i<iFilterLen; i++)
- bTab[i] = bArray[i];
- int i, j, bTemp;
- for (j = 0; j < iFilterLen - 1; j++)
- {
- for (i = 0; i < iFilterLen - j - 1; i++)
- {
- if (bTab[i] > bTab[i + 1])
- {
- bTemp = bTab[i];
- bTab[i] = bTab[i + 1];
- bTab[i + 1] = bTemp;
- }
- }
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++){
+    for (i = 0; i < iFilterLen - j - 1; i++){
+      if (bTab[i] > bTab[i + 1]){
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
  }
  if ((iFilterLen & 1) > 0)
- bTemp = bTab[(iFilterLen - 1) / 2];
+  bTemp = bTab[(iFilterLen - 1) / 2];
  else
- bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
  return bTemp;
 } 
+
 void loop() {
   // reset flag
   flag = true;
   
   // read/save data from pH sensor
-  data.pH = 1.0;
+  measure = analogRead(ph_pin);
+  voltage = 5 / 1024.0 * measure;
+  po = 7 + ((2.5 - voltage) / 0.18);
+  data.pH = po;
+  Serial.println(data.pH);
 
   // read/save data from temp sensor
   Serial.println(sensors.getAddress(t_address, 0));
