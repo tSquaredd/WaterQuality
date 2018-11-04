94dc402f6af54a861b2823002a1c697c9c340e8a
Miles <milesave@buffalo.edu>
2018-11-04 01:54:40 -0400

    temp probe working
    added support for temp probe, pushing data successfully

--- arduino/test_mega/test_mega.ino
+++ arduino/test_mega/test_mega.ino
@@ -1,5 +1,12 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <DS3231.h>
 
+// temp sensor
+#define ONE_WIRE_BUS 22
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+// real time clock
 DS3231 rtc(SDA,SCL);
 
 Time t;
@@ -30,13 +37,18 @@ void setup() {
 
   //start real time clock
   rtc.begin();
-
+  sensors.begin();
   //delay
   delay(5000);
-  
-  //rtc.setTime(5,30,0);
-  //rtc.setDate(3,11,2018);
+  Serial.println("startup complete");
+  //rtc.setTime(0,11,0);
+  //rtc.setDate(4,11,2018);
 }
+
+// start up Dallas library
+
+
+DeviceAddress t_address;
 
 void loop() {
   // reset flag
@@ -51,16 +63,24 @@ void loop() {
   data.tds = 1.0;
   
   // read/save data from temp sensor
-  data.temp = 1.0;
+  Serial.println(sensors.getAddress(t_address, 0));
+  sensors.requestTemperatures();
+  data.temp = sensors.getTempC(t_address);
+  Serial.println(data.temp);
   
   // read/save data from flow meter
   data.flow = 1.0;
   
   // read time from rtc and insert into packet
+  Serial.println("getting real time");
   t = rtc.getTime();
   data.h = t.hour;
   data.m = t.min;
   data.s = t.sec;
+  Serial.print(data.h);
+  Serial.print(data.m);
+  Serial.print(data.s);
+  Serial.println();
   data.y = t.year;
   data.mth = t.mon;
   data.d = t.date;
@@ -81,13 +101,14 @@ void loop() {
   while(flag){
     if(msg == "bad"){
       // resend data
-      Serial.print("sending packet..");
+      Serial.println("sending packet..");
       Serial1.write(buff, PACKET_SIZE);
       // wait for response
       while(!Serial1.available()){
       };
       msg = Serial1.readString(); 
     } else if(msg == "sent"){
+      Serial.println("send successful");
       flag = false; 
     } else {
       Serial.println("something is badddd");
@@ -96,5 +117,5 @@ void loop() {
   }
   
   // Wait three seconds before repeating :)
-  delay (3000); 
+  delay (1000); 
 }
